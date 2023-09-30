f063c7b4f21a22b2b533419ad0bcd685252595e4
Nicholas Arvelo <info@nicholasarvelo.com>
2023-09-30 17:59:12 -0400

    Just some linting

--- main.go
+++ main.go
@@ -13,8 +13,8 @@ import (
 
 func main() {
 	// Retrieve Cloudflare API key and DNS record from environment variables
-	CloudflareAPIKey := os.Getenv("CLOUDFLARE_API_KEY")
-	CloudflareDNSRecord := os.Getenv("CLOUDFLARE_DNS_RECORD")
+	cloudflareAPIKey := os.Getenv("CLOUDFLARE_API_KEY")
+	cloudflareDNSRecord := os.Getenv("CLOUDFLARE_DNS_RECORD")
 	cloudflareDNSRecordType := os.Getenv("CLOUDFLARE_DNS_RECORD_TYPE")
 
 	currentPublicIP, errorOccured := queryPublicIP()
@@ -26,13 +26,13 @@ func main() {
 	ctx := context.Background()
 
 	// Split the Cloudflare DNS record into its components
-	_, cloudflareZoneName, errorOccurred := splitRecord(CloudflareDNSRecord)
+	_, cloudflareZoneName, errorOccurred := splitRecord(cloudflareDNSRecord)
 	if errorOccurred != nil {
 		log.Fatalln(errorOccurred)
 	}
 
 	// Create a Cloudflare API client using the API key
-	apiClient, errorOccurred := cloudflare.NewWithAPIToken(CloudflareAPIKey)
+	apiClient, errorOccurred := cloudflare.NewWithAPIToken(cloudflareAPIKey)
 	if errorOccurred != nil {
 		log.Println(errorOccurred)
 	}
@@ -48,7 +48,7 @@ func main() {
 	zoneRecord, _, errorOccurred := apiClient.ListDNSRecords(
 		ctx,
 		cloudflareZoneID,
-		cloudflare.ListDNSRecordsParams{Name: CloudflareDNSRecord},
+		cloudflare.ListDNSRecordsParams{Name: cloudflareDNSRecord},
 	)
 	if errorOccurred != nil {
 		log.Println("Failed to list records:", errorOccurred)
@@ -59,7 +59,7 @@ func main() {
 		_, errorOccurred = apiClient.CreateDNSRecord(
 			ctx, cloudflareZoneID, cloudflare.CreateDNSRecordParams{
 				Type:      cloudflareDNSRecordType,
-				Name:      CloudflareDNSRecord,
+				Name:      cloudflareDNSRecord,
 				Content:   currentPublicIP,
 				Comment:   "LFGoD2NS-Cloudflare",
 				Proxiable: true,
@@ -71,14 +71,14 @@ func main() {
 		} else {
 			log.Printf(
 				"Record Created: '%s' is resolving to '%s'",
-				CloudflareDNSRecord,
+				cloudflareDNSRecord,
 				currentPublicIP,
 			)
 		}
 	} else {
 		_, errorOccurred = apiClient.UpdateDNSRecord(
 			ctx, cloudflareZoneID, cloudflare.UpdateDNSRecordParams{
-				Name:    CloudflareDNSRecord,
+				Name:    cloudflareDNSRecord,
 				Content: currentPublicIP,
 				ID:      zoneRecord[0].ID,
 			},
@@ -88,7 +88,7 @@ func main() {
 		} else {
 			log.Printf(
 				"Record Updated: '%s' is resolving to '%s'",
-				CloudflareDNSRecord,
+				cloudflareDNSRecord,
 				currentPublicIP,
 			)
 		}
@@ -131,14 +131,17 @@ func queryPublicIP() (string, error) {
 
 // split function takes a Cloudflare DNS record and splits it into recordName and zoneName.
 // It returns these components as strings and an error if the DNS record is invalid.
-func splitRecord(CloudflareDNSRecord string) (string, string, error) {
-	splitDNSRecord := strings.Split(CloudflareDNSRecord, ".")
+func splitRecord(cloudflareDNSRecord string) (string, string, error) {
+	splitDNSRecord := strings.Split(cloudflareDNSRecord, ".")
 	if len(splitDNSRecord) != 3 {
-		return "", "", fmt.Errorf("invalid DNS Record: %s", CloudflareDNSRecord)
+		return "", "", fmt.Errorf("invalid DNS Record: %s", cloudflareDNSRecord)
 	}
 
 	recordName := splitDNSRecord[0]
-	zoneName := strings.Join([]string{splitDNSRecord[1], splitDNSRecord[2]}, ".")
+	zoneName := strings.Join(
+		[]string{splitDNSRecord[1], splitDNSRecord[2]},
+		".",
+	)
 
 	return recordName, zoneName, nil
 }
